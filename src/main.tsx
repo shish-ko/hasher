409d636b81ffe06f3689080fe24d690253d11b2d
shish-ko <andrey.shishko@gmail.com>
2023-11-28 16:48:11 +0300

    feat: ssr

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { router } from 'router/router.tsx';
+import { routeObj } from 'router/router.tsx';
 import { store } from 'store/store';
 import './style/index.scss';
-import { RouterProvider } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { injectStore } from '~utils/helpers';
 
 injectStore(store);
+const router = createBrowserRouter(routeObj);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.hydrateRoot(document.getElementById('root')!,
   // <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { SERVER_URL } from 'constants';
+import { SERVER_URL } from '../constants';
 import { loader } from '~utils/helpers';
 
 export const serverAPI = axios.create({baseURL: SERVER_URL});
