5f5093b82ccca660fbef89c3e2d06ab3a05c32cb
shish-ko <shishko.dev@gamil.com>
2023-12-20 15:14:48 +0300

    refactor: header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,29 +1,27 @@
 import React from "react";
 import { Auth } from "./Auth";
-import { Box, List, ListItem, Stack, Typography, useTheme } from "@mui/material";
+import { List, ListItem, Stack, Typography, useTheme } from "@mui/material";
 import logo from '../../assets/Logo.png';
-
+import { Link } from "react-router-dom";
 
 export const Header: React.FC = () => {
   const theme = useTheme();
+
   return (
-    <Stack component={'header'} direction={'row'} padding={theme.spacing(3, 5)} alignSelf={'stretch'}>
+    <Stack component={'header'} direction={'row'} padding={theme.spacing(4, 5)} height={'64px'} borderBottom={'1px solid rgba(0,0,0, 0.07)'}>
       <Stack direction='row' alignItems='center' spacing={5}>
-        <img src={logo} alt="app logo" className="header__logo"/>
+        <img src={logo} alt="app logo" className="header__logo" />
         <Typography variant="h1">SecretService {theme.spacing()}</Typography>
       </Stack>
-      <List disablePadding sx={{display: "flex", gap: theme.spacing(5), justifyContent: "flex-end", flex: '1 0 0'}}>
-        <ListItem disablePadding>Terms and conditions</ListItem>
-        <ListItem disablePadding>About us</ListItem>
-        <ListItem disablePadding>Authorization</ListItem>
+      <List disablePadding sx={{ display: "flex", gap: theme.spacing(5), justifyContent: "flex-end", flex: '1 0 0' }}>
+        <ListItem disablePadding sx={{ width: 'fit-content' }}>
+          <Link to={'#'}><Typography variant="appNav">Terms and conditions</Typography></Link>
+        </ListItem>
+        <ListItem disablePadding sx={{ width: 'fit-content' }}>
+          <Link to={'#'}><Typography variant="appNav">About us</Typography></Link>
+        </ListItem>
+        <Auth />
       </List>
-      {/* <header className="app-container header">
-        <div className="header__item"></div>
-        <h1 className="header__item header__item_center">Hasher</h1>
-        <div className="header__item header__item_right">
-          <Auth/> 
-        </div>
-      </header> */}
     </Stack>
   );
 };
---
+++ src/style/colors.ts
@@ -0,0 +1,3 @@
+export const COLORS = {
+  appNav: '#444444',
+};
