8ecbc6dc9f07fdd1561839730d8e78883fb7d0b7
shish-ko <shishko.dev@gamil.com>
2023-12-26 00:48:11 +0300

    refactor: loader component

--- src/pages/Auth/AuthCheckingRoutes.tsx
+++ src/pages/Auth/AuthCheckingRoutes.tsx
@@ -1,28 +1,58 @@
 import { useAuth } from "~utils/hooks";
-import {useState, useEffect, CSSProperties} from 'react';
+import { useState, useEffect, CSSProperties } from 'react';
 import { loader } from '~utils/helpers';
 import { PropagateLoader } from "react-spinners";
 import { LOADER_COLOR } from "../../constants";
 import { Outlet } from "react-router-dom";
+import { Box, Container, Stack, Typography } from "@mui/material";
+import spinner from '../../assets/spinner.png';
+
+const titleStyle: CSSProperties = {
+  fontFamily: 'Bowlby One',
+  fontSize: '52px',
+  textTransform: 'uppercase',
+  lineHeight: '52px',
+  color: 'black',
+};
+const subTitleStyle: CSSProperties = {
+  fontSize: '24px',
+  textTransform: 'uppercase',
+  lineHeight: '24px',
+  color: 'black',
+  letterSpacing: '19px',
+};
 
 export const AuthCheckingRoutes: React.FC = () => {
-  const {setUser} = useAuth();
+  const { setUser } = useAuth();
   const [isChecking, setIsChecking] = useState(true);
 
-  useEffect(() => {
-    async function authChecker() {
-      const token = await loader();
-      if(token) {
-        setUser(token);
-      }
-      setIsChecking(false);
-    }
-    authChecker();
-  }, []);
+  // useEffect(() => {
+  //   async function authChecker() {
+  //     const token = await loader();
+  //     if(token) {
+  //       setUser(token);
+  //     }
+  //     setIsChecking(false);
+  //   }
+  //   authChecker();
+  // }, []);
 
   return (
     isChecking ?
-      <PropagateLoader color={LOADER_COLOR} size={23}/> 
+      <Box sx={{ backgroundColor: 'white', height: '100vh', position: 'fixed', top: 0, right: 0, left: 0 }}>
+        <Container >
+          <Stack direction='row' alignItems='center' justifyContent='space-around' >
+            <Stack gap={5} >
+              <Typography sx={titleStyle}>Secret Service</Typography>
+              <Typography sx={subTitleStyle}>confidentiality</Typography>
+            </Stack>
+            <Stack alignItems='center' > 
+              <img src={spinner} className="authSpinner"></img>
+            </Stack>
+          </Stack>
+        </Container>
+
+      </Box>
       :
       <Outlet />
   );
