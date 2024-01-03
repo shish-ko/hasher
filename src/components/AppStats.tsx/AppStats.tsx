81f97ee9a89671cc19d40745485ca6e3f77a7b70
shish-ko <andrey.shishko@gmail.com>
2024-01-03 16:57:03 +0300

    fix: AppInput, AppButton

--- src/components/AppStats.tsx/AppStats.tsx
+++ src/components/AppStats.tsx/AppStats.tsx
@@ -13,11 +13,11 @@ export const AppStats: React.FC = () => {
       <Stack justifyContent='space-evenly' direction='row'>
         <Stack alignItems='center'>
           <CountUp end={30} style={{ ...theme.typography.h3, color: 'black', marginBottom: theme.spacing(2) }} />
-          <Typography variant="h3" color='black'>users</Typography>
+          <Typography variant="h3" color='black' m={0}>users</Typography>
         </Stack>
         <Stack alignItems='center'>
           <CountUp end={20} style={{ ...theme.typography.h3, color: 'black', marginBottom: theme.spacing(2) }} />
-          <Typography variant="h3" color='black'>secrets</Typography>
+          <Typography variant="h3" color='black' m={0}>secrets</Typography>
         </Stack>
       </Stack>
     </AppBlock>
--- src/components/UI_components/Button/index.tsx
+++ src/components/UI_components/Button/index.tsx
@@ -1,13 +1,17 @@
-import { Button, styled } from "@mui/material";
+import { Button, darken, styled } from "@mui/material";
 import { COLORS } from "style/colors";
 
 export const AppButton = styled(Button)(({theme})=> {
   return {
     fontSize: theme.spacing(4),
+    lineHeight: theme.spacing(4),
     borderRadius: '10px',
     textTransform: 'none',
     padding: '15px',
     backgroundColor: COLORS.lightBG,
-    color: 'black'
+    color: 'black',
+    '&:hover': {
+      backgroundColor: darken(COLORS.lightBG, .2)
+    }
   };
 });
