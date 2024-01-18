0d5375bfa078a496c40e32d2a9edf2dee3c78418
shish-ko <andrey.shishko@gmail.com>
2024-01-18 16:40:19 +0300

    refactor: PopUp on MUI comps

--- src/components/UI_components/PopUp/PopUp.tsx
+++ src/components/UI_components/PopUp/PopUp.tsx
@@ -1,23 +1,15 @@
-import React, { useEffect } from "react";
+import { Alert, Snackbar } from "@mui/material";
+import React from "react";
 import { hidePopUp } from "store/popUpSlice";
 import { useAppDispatch, useAppSelector } from "utils/hooks";
 
 export const PopUp: React.FC = () => {
   const { isActive, message, type } = useAppSelector((store) => store.popUp);
   const dispatch = useAppDispatch();
-  useEffect(() => {
-    if (isActive) {
-      setTimeout(() => dispatch(hidePopUp()), 2500);
-    }
-  }, [isActive]);
   return (
-    isActive ?
-      <section className={`popUp popUp_${type} popUp_active`}>
-        <h3 className="popUp__msg">{message}</h3>
-      </section> :
-      <section className={`popUp popUp_${type}`}>
-        <h3 className="popUp__msg">{message}</h3>
-      </section>
+    <Snackbar open={isActive} onClose={() => dispatch(hidePopUp())} autoHideDuration={2500}>
+      <Alert severity={type}>{message}</Alert>
+    </Snackbar>
   );
 };
 
