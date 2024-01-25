3686bd6f9c053b01a02d92540376a42cf7683516
shish-ko <shishko.dev@gamil.com>
2024-01-26 00:49:49 +0300

    fix: User useEffect

--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -1,5 +1,5 @@
 import { SERVER_URL } from "app_constants";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { LoaderFunctionArgs, useParams } from "react-router-dom";
 import { SecretsList } from "~comps/Secrets/SecretList";
 import { AppBlock } from "~comps/UI_components/AppBlock/AppBlock";
@@ -10,13 +10,20 @@ import { useAppSelector, usePopUp, useServerFetch } from "~utils/hooks";
 
 export const User: React.FC = () => {
   const { userId } = useParams<Record<IRouterParams, string>>();
-  // eslint-disable-next-line prefer-const
-  let {res: secrets, refetch} = useServerFetch<IUserFetchRes>(`user/${userId}`);
   const { newSecrets } = useAppSelector((state) => state.user);
+  const hasPageBeenRendered = useRef(false);
+  // eslint-disable-next-line prefer-const
+  let { res: secrets, refetch } = useServerFetch<IUserFetchRes>(`user/${userId}`);
   if (import.meta.env.VITE_AUTH_FREE) {
     secrets = get_MOCK_USER_SECRETS();
   }
-  useEffect(()=> {refetch();}, [newSecrets]);
+  useEffect(() => {
+    if (hasPageBeenRendered.current) {
+      refetch();
+      return;
+    }
+    hasPageBeenRendered.current = true;
+  }, [newSecrets]);
 
   return (
     <AppBlock component='section'>
