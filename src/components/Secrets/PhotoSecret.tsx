e50658337acb3765b1f6233366ccf93dce73a685
shish-ko <shishko.dev@gamil.com>
2024-04-09 14:38:18 +0300

    fix: image appearance at small secret

--- src/components/Secrets/PhotoSecret.tsx
+++ src/components/Secrets/PhotoSecret.tsx
@@ -1,10 +1,14 @@
 import { Box } from "@mui/material";
 import { SERVER_URL } from "app_constants";
+import { useEffect, useState } from "react";
 import { ISecretProps } from "~interfaces/index";
 
 export const PhotoSecret: React.FC<ISecretProps> = ({url}) => {
-  const img = new Image();
-  img.src = SERVER_URL + url;
-  const isImageOversize = img.height > 200;
+  const [isImageOversize, setIsImageOversize] = useState(false);
+  useEffect(()=> {
+    const img = new Image();
+    img.addEventListener('load', ()=>{if(img.height > 200) setIsImageOversize(true);});
+    img.src = SERVER_URL + url;
+  }, [url]);
   return <Box sx={{position: 'relative', background: `center no-repeat url(${SERVER_URL + url})`, backgroundSize: `${isImageOversize ? 'contain' : 'auto'}`, minHeight: '200px'}}/>;
 };
